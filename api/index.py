af1a456abc6a6b7cf79782b699ffe13250e44aab
Shaik Riyan Shareef <RIYAN46SHAREEF@GMAIL.COM>
2026-01-26 17:51:25 +0530

    Fix routing setup

--- api/index.py
+++ api/index.py
@@ -3,16 +3,13 @@ import os
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PUBLIC_DIR = os.path.join(BASE_DIR, "..", "public")
+
 @app.route("/")
 def home():
-    return send_from_directory(
-        os.path.join(os.getcwd(), "public"),
-        "index.html"
-    )
+    return send_from_directory(PUBLIC_DIR, "index.html")
 
 @app.route("/<path:path>")
 def static_files(path):
-    return send_from_directory(
-        os.path.join(os.getcwd(), "public"),
-        path
-    )
+    return send_from_directory(PUBLIC_DIR, path)
